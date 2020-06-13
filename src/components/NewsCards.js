f5ced0ea6e07672fc79fa583b51e8b63cc0df4e2
adrianhajdin <adrianhajdin00@gmail.com>
2020-06-13 10:28:16 +0200

    animations

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -6,36 +6,54 @@ import { makeStyles } from '@material-ui/core/styles';
 import NewsCard from './NewsCard';
 
 const useStyles = makeStyles({
-  withChildren: {
-    visibility: 'initial !important',
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    height: '57vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'white',
   },
 });
+
+const skeletons = [
+  { color: '#00838f', title: 'Latest news', text: '"Give me the latest news"' },
+  { color: '#1565c0', title: 'News in a Category', text: '"Give me the latest [Technology] news"' },
+  { color: '#4527a0', title: 'News by Term', text: '"What\'s up with [Playstation 5]"' },
+  { color: '#283593', title: 'News from a Source', text: '"Give me the news from [CNN]"' },
+];
 
 const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles();
 
   if (!articles.length) {
     return (
-      <Grid style={{ padding: '0 10%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
-        {[...Array(4).keys()].map(() => (
-          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
-            {/* <Skeleton variant="text" classes={{ withChildren: classes.withChildren }} style={{ dislay: 'flex', alignItems: 'center', justifyContent: 'center' }} animation="wave" variant="rect" width="100%" height="57vh">
-              test
-              <Typography style={{ visibility: 'initial !important' }} variant="h5" component="h2">Try saying: "Give me the latest news!"</Typography>
+      <Grow in>
 
-            </Skeleton> */}
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '57vh', backgroundColor: '#e2e2e2', padding: '10%', borderRadius: 10 }}>
-              <Typography variant="h5" component="h2">Try saying: "Give me the latest news!"</Typography>
-            </div>
-          </Grid>
-        ))}
-      </Grid>
+        <Grid style={{ padding: '0 5%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
+          {skeletons.map((skeleton) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+              <div
+                className={classes.card}
+                style={{ backgroundColor: skeleton.color,
+                }}
+              >
+                <Typography variant="h5" component="h5">{skeleton.title}</Typography>
+                <Typography variant="h6" component="h6">Try saying: <br /> {skeleton.text}</Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
     );
   }
 
   return (
     <Grow in>
-      <Grid style={{ padding: '0 10%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
+      <Grid style={{ padding: '0 5%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
             <NewsCard activeArticle={activeArticle} i={i} article={article} />
