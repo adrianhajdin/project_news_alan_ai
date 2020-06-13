7549dbb219de1046eef4efa3f94614b3e7f331bb
adrianhajdin <adrianhajdin00@gmail.com>
2020-06-13 09:31:44 +0200

    design

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import classNames from 'classnames';
 
 const useStyles = makeStyles({
   media: {
-    height: 300,
+    height: 250,
   },
   border: {
     border: 'solid',
@@ -13,43 +14,52 @@ const useStyles = makeStyles({
     height: '100%',
   },
   card: {
-    margin: 16,
-    padding: '0 !important',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
+    borderBottom: '10px solid white',
   },
-  date: {
-    margin: '16px 0 0 16px !important',
+  activeCard: {
+    borderBottom: '10px solid #22289a',
   },
-  source: {
-    margin: '16px 16px 0 0 !important',
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
   },
 });
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
   const classes = useStyles();
-
   return (
-    <Grid item xs={12} lg={3} style={{ display: 'flex', border: activeArticle === i ? '2px solid red' : null }}>
-      <Card style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
-        <CardActionArea href={url}>
-          <CardMedia className={classes.media} image={urlToImage} title={title} />
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography className={classes.date} variant="body2" color="textSecondary" component="h2">{`${(new Date(publishedAt)).toDateString()} ${(new Date(publishedAt)).toLocaleTimeString()}`}</Typography>
-            <Typography className={classes.source} variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
-          </div>
-          <Typography style={{ paddingLeft: '16px' }} gutterBottom variant="h5" component="h2">{title}</Typography>
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">Learn More</Button>
-          <Typography variant="body2" color="textSecondary" component="h2">{i + 1}</Typography>
-        </CardActions>
-      </Card>
-    </Grid>
+    <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+      <CardActionArea href={url}>
+        <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} title={title} />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" href={url}>Learn More</Button>
+        <Typography variant="h5" color="textSecondary" component="h2">{i + 1}</Typography>
+      </CardActions>
+    </Card>
   );
 };
 
--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Grow, Typography } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import NewsCard from './NewsCard';
 
 const NewsCards = ({ articles, activeArticle }) => {
   if (!articles.length) {
+    return (
+      <Grid style={{ padding: '0 10%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
+        {/* <Skeleton animation="wave" variant="rect" width={210} height={118} /> */}
+
+        {[...Array(4).keys()].map(() => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+            <Skeleton animation="wave" variant="rect" width="100%" height="57vh" />
+            <Typography variant="body2" color="textSecondary" component="h2">Try saying: "Give me the latest news!"</Typography>
+          </Grid>
+        ))}
+      </Grid>
+    );
     return <h1>Give me a command.</h1>;
   }
 
   return (
-    <Grid style={{ padding: '0 10%' }} container alignItems="stretch" spacing={3}>
-      {articles.map((article, i) => (
-        <NewsCard activeArticle={activeArticle} i={i} article={article} />
-      ))}
-    </Grid>
+    <Grow in>
+      <Grid style={{ padding: '0 10%', width: '100%', margin: 0 }} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard activeArticle={activeArticle} i={i} article={article} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 
